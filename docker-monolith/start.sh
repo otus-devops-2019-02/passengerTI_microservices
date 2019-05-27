43aea3e5a165b669ea841bc2942602c8845fee8e
root <root@sc.semenushkin.ru>
2019-05-28 01:06:56 +0300

    Add fixes for travis

--- docker-monolith/start.sh
+++ docker-monolith/start.sh
@@ -5,3 +5,5 @@
 source /reddit/db_config
 
 cd /reddit && puma || exit
+
+
--- firewall.sh
+++
@@ -1,1 +0,0 @@
-gcloud compute firewall-rules create reddit-app --allow tcp:9292 --target-tags=docker-machine --description="Allow PUMA connections" --direction=INGRESS
